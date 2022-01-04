ece70a62adceaff99be0197ef65f6427f60008b1
Rónán Carrigan <rcarriga@tcd.ie>
2022-01-04 09:29:15 +0000

    feat: dynamic descriptions

--- init.lua
+++ init.lua
@@ -1,62 +1,76 @@
 _G._run_tests = function(args)
-  local filter = args.filter or {}
-  local file = args.file
-  local results
-  local code
-
-  local busted = require("plenary.busted")
-  local base_format = busted.format_results
-  local base_it = busted.it
-  local base_describe = busted.describe
-  local base_inner_describe = busted.inner_describe
   local base_exit = os.exit
-  os.exit = function(code_)
-    code = code_
-  end
-  busted.format_results = function(results_)
-    results = results_
-    return base_format(results)
-  end
-  busted.describe = function(desc, ...)
-    if #filter > 0 then
-      if filter[1] ~= desc then
-        return
-      end
-      table.remove(filter, 1)
+  local success = pcall(function()
+    local filters = args.filter or {}
+    local file = args.file
+    local results
+    local code
+
+    local busted = require("plenary.busted")
+    local base_format = busted.format_results
+    local base_it = busted.it
+    local base_describe = busted.describe
+    local base_inner_describe = busted.inner_describe
+    local func_locations = {}
+    os.exit = function(code_)
+      code = code_
+    end
+    busted.format_results = function(results_)
+      results = results_
+      return base_format(results)
     end
 
-    return base_describe(desc, ...)
-  end
-  busted.inner_describe = function(desc, ...)
-    if #filter > 0 then
-      if filter[1] ~= desc then
-        return
-      end
-      table.remove(filter, 1)
+    local function add_to_locations(desc, func)
+      local info = debug.getinfo(func, "S")
+      func_locations[desc] = info.linedefined - 1
     end
 
-    return base_inner_describe(desc, ...)
-  end
-  busted.it = function(desc, ...)
-    if #filter > 0 then
-      if filter[1] ~= desc then
-        return
+    local function filter(func)
+      if #filters > 0 then
+        local filter_start, filter_end = unpack(filters[1])
+        local func_info = debug.getinfo(func, "S")
+        local func_start, func_end = func_info.linedefined - 1, func_info.lastlinedefined - 1
+        if filter_start > func_start or filter_end < func_end then
+          return false
+        end
+        table.remove(filters, 1)
       end
-      -- If a test passes the final filter we don't want other tests running
-      if #filter == 1 then
-        filter = { 0 }
-      else
-        table.remove(filter, 1)
-      end
+      return true
     end
 
-    return base_it(desc, ...)
-  end
+    busted.describe = function(desc, func)
+      if not filter(func) then
+        return
+      end
+      add_to_locations(desc, func)
 
-  vim.cmd("runtime plugin/plenary.vim")
-  require("plenary.busted").run(file)
-  local results_file = io.open(args.results, "w")
-  results_file:write(vim.json.encode(results))
-  results_file:close()
-  base_exit(code)
+      return base_describe(desc, func)
+    end
+    busted.inner_describe = function(desc, func)
+      if not filter(func) then
+        return
+      end
+      add_to_locations(desc, func)
+
+      return base_inner_describe(desc, func)
+    end
+    busted.it = function(desc, func)
+      if not filter(func) then
+        return
+      end
+      add_to_locations(desc, func)
+
+      return base_it(desc, func)
+    end
+
+    it = busted.it
+    describe = busted.describe
+
+    require("plenary.busted").run(file)
+    local results_file = assert(io.open(args.results, "w"))
+    results_file:write(vim.json.encode({ results = results, locations = func_locations }))
+    results_file:close()
+    base_exit(code)
+  end)
+  base_exit(1)
 end
--- lua/neotest-plenary/init.lua
+++ lua/neotest-plenary/init.lua
@@ -8,6 +8,20 @@ local function script_path()
   return str:match("(.*/)")
 end
 
+local function join_results(base, update)
+  if not base then
+    return update
+  end
+  local status = (base.status == "failed" or update == "failed") and "failed" or "passed"
+  local errors = (base.errors or update.errors)
+      and (vim.list_extend(base.errors or {}, update.errors or {}))
+    or nil
+  return {
+    status = status,
+    errors = errors,
+  }
+end
+
 local test_script = (Path.new(script_path()):parent():parent() / "run_tests.sh").filename
 
 ---@type NeotestAdapter
@@ -24,19 +38,15 @@ function PlenaryNeotestAdapter.discover_positions(path)
     local query = [[
       ((function_call
         (identifier) @func_name
-        (arguments (string) @namespace.name )
+        (arguments (_) @namespace.name (function_definition))
       ) (#match? @func_name "describe")) @namespace.definition
 
       ((function_call
         (identifier) @func_name
-        (arguments (string) @test.name )
+        (arguments (_) @test.name (function_definition))
       ) (#match? @func_name "it")) @test.definition
     ]]
-    return lib.treesitter.parse_positions(
-      path,
-      query,
-      { quoted_names = true, nested_namespaces = true }
-    )
+    return lib.treesitter.parse_positions(path, query, { nested_namespaces = true })
   end
   local files = lib.func_util.filter_list(base.is_test_file, lib.files.find({ path }))
   return lib.files.parse_dir_from_files(path, files)
@@ -46,15 +56,23 @@ end
 ---@return NeotestRunSpec | nil
 function PlenaryNeotestAdapter.build_spec(args)
   local results_path = vim.fn.tempname()
-  local pos = args.position
-  if not pos or pos.type == "dir" then
+  local tree = args.tree
+  if not tree then
+    return
+  end
+  local pos = args.tree:data()
+  if pos.type == "dir" then
     return
   end
   local filters = {}
   if pos.type == "namespace" or pos.type == "test" then
-    table.insert(filters, 1, pos.name)
-    for i = #pos.namespaces, 1, -1 do
-      table.insert(filters, 1, pos.namespaces[i])
+    table.insert(filters, 1, { pos.range[1], pos.range[3] })
+    for parent in tree:iter_parents() do
+      local parent_pos = parent:data()
+      if parent_pos.type ~= "namespace" then
+        break
+      end
+      table.insert(filters, 1, { parent_pos.range[1], parent_pos.range[3] })
     end
   end
   local script_args = vim.tbl_flatten({
@@ -85,6 +103,23 @@ local function convert_plenary_result(result, status, file)
     }
 end
 
+---@param lists string[][]
+local function permutations(lists, cur_i)
+  cur_i = cur_i or 1
+  if cur_i > #lists then
+    return { {} }
+  end
+  local sub_results = permutations(lists, cur_i + 1)
+  local result = {}
+  for _, elem in pairs(lists[cur_i]) do
+    for _, sub_result in pairs(sub_results) do
+      local l = vim.list_extend({ elem }, sub_result)
+      table.insert(result, l)
+    end
+  end
+  return result
+end
+
 ---@class PlenaryTestResult
 ---@field descriptions string[]
 ---@field msg? string
@@ -95,18 +130,29 @@ end
 ---@field errs PlenaryTestResult[]
 ---@field fatal PlenaryTestResult[]
 
+---@class PlenaryOutput
+---@field results PlenaryTestResults
+---@field locations table<string, integer>
+
 ---@async
 ---@param spec NeotestRunSpec
----@param result NeotestStrategyResult
+---@param _ NeotestStrategyResult
+---@param tree Tree
 ---@return NeotestResult[]
-function PlenaryNeotestAdapter.results(spec, result)
+function PlenaryNeotestAdapter.results(spec, _, tree)
   -- TODO: Find out if this JSON option is supported in future
   local success, data = pcall(lib.files.read, spec.context.results_path)
   if not success then
     data = vim.json.encode({ pass = {}, fail = {}, errs = {}, fatal = {} })
   end
-  ---@type PlenaryTestResults
-  local plenary_results = vim.json.decode(data, { luanil = { object = true } })
+  ---@type PlenaryOutput
+  local plenary_output = vim.json.decode(data, { luanil = { object = true } })
+  if not plenary_output.results then
+    return {}
+  end
+
+  local plenary_results = plenary_output.results
+  local locations = plenary_output.locations
   local results = {}
   for _, plen_result in pairs(plenary_results.pass) do
     local pos_id, pos_result = convert_plenary_result(plen_result, "passed", spec.context.file)
@@ -116,13 +162,53 @@ function PlenaryNeotestAdapter.results(spec, result)
   local failed = vim.list_extend({}, plenary_results.errs)
   vim.list_extend(failed, plenary_results.fail)
   vim.list_extend(failed, plenary_results.fatal) -- TODO: Verify shape
+
   for _, plen_result in pairs(failed) do
     local pos_id, pos_result = convert_plenary_result(plen_result, "failed", spec.context.file)
     results[pos_id] = pos_result
     file_result.status = "failed"
     vim.list_extend(file_result.errors, pos_result.errors)
   end
+
   results[spec.context.file] = file_result
+
+  --- We now have all results mapped by their alias names
+  --- Need to combine using alias map
+
+  local aliases = {}
+  for alias, line in pairs(locations) do
+    local node = tree:sorted_search(line, function(pos)
+      return pos.range[1]
+    end, false)
+    if not node then
+      error("Node not found for line " .. line)
+    end
+    local pos = node:data()
+    aliases[pos.id] = aliases[pos.id] or {}
+    table.insert(aliases[pos.id], alias)
+  end
+
+  for _, node in tree:iter_nodes() do
+    local pos = node:data()
+    if pos.type == "test" and not results[pos.id] then
+      local namespace_aliases = {}
+      for parent in node:iter_parents() do
+        if parent:data().type ~= "namespace" then
+          break
+        end
+        table.insert(namespace_aliases, 1, aliases[parent:data().id])
+      end
+      local namespace_permutations = permutations(namespace_aliases)
+      for _, perm in ipairs(namespace_permutations) do
+        for _, alias in ipairs(aliases[pos.id]) do
+          local alias_id = table.concat(vim.tbl_flatten({ pos.path, perm, alias }), "::")
+          results[pos.id] = join_results(results[pos.id], results[alias_id])
+          results[alias_id] = nil
+        end
+      end
+    end
+  end
+
   return results
 end
 
