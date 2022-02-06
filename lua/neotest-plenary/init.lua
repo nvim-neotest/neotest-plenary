498de2d2a193e710d5f378eb66e0a76d04077629
Rónán Carrigan <rcarriga@tcd.ie>
2022-02-06 10:29:16 +0000

    feat: queries for new lua treesitter

--- lua/neotest-plenary/init.lua
+++ lua/neotest-plenary/init.lua
@@ -38,20 +38,27 @@ end
 ---@return Tree | nil
 function PlenaryNeotestAdapter.discover_positions(path)
   local query = [[
-      ((function_call
-        (identifier) @func_name
-        (arguments (_) @namespace.name (function_definition))
-      ) (#match? @func_name "^describe$")) @namespace.definition
+  ;; describe blocks
+  ((function_call
+      name: (identifier) @func_name (#match? @func_name "^describe$")
+      arguments: (arguments (_) @namespace.name (function_definition))
+  )) @namespace.definition
 
-      ((function_call
-        (identifier) @func_name
-        (arguments (_) @test.name (function_definition))
-      ) (#match? @func_name "^it$")) @test.definition
 
-      ((function_call
-        (field_expression (_) (property_identifier) @func_name)
-        (arguments (_) @test.name (function_definition))
-      ) (#match? @func_name "^it$")) @test.definition
+  ;; it blocks
+  ((function_call
+      name: (identifier) @func_name
+      arguments: (arguments (_) @test.name (function_definition))
+  ) (#match? @func_name "^it$")) @test.definition
+
+  ;; async it blocks (async.it)
+  ((function_call
+      name: (
+        dot_index_expression 
+          field: (identifier) @func_name
+      )
+      arguments: (arguments (_) @test.name (function_definition))
+    ) (#match? @func_name "^it$")) @test.definition
     ]]
   return lib.treesitter.parse_positions(path, query, { nested_namespaces = true })
 end
