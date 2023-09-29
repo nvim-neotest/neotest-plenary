nvim-neotest/neotest-plenary

commit ffc33b0ba95aa809df085150cf0b64bc0e5a4b4c

feat: automatically inject plenary.nvim runtimepath (#10)
Currently plenary adapters won't work out of box because plenary.nvim is
not added to vim &runtimepath or lua package.path (#4). Without any
config, test will fail with the error:

    .../neotest-plenary/run_tests.lua:26: module 'plenary.busted' not found:
    ^Ino field package.preload['plenary.busted']

Instead, we can get the path where plenary.nvim is installed and inject
the path when running a headless neovim instance for testing. With this,
neotest-plenary can run plenary.busted tests out-of-box even if users
do not configure `{ min_init = ... }`.

Also add some sensible options (no shada, no swapfile) to the command
line.

Fixes #4.

lua/neotest-plenary/init.lua
@@ -3,6 +3,12 @@ local Path = require("plenary.path")
 local lib = require("neotest.lib")
 local base = require("neotest-plenary.base")
 
+-- the local path to the plenary.nvim plugin installed
+---@type string
+local plenary_dir = vim.fn.fnamemodify(
+  debug.getinfo(require("plenary.busted").run).source:match("@?(.*[/\\])"), ":p:h:h:h"
+)
+
 local config = {
   min_init = nil,
 }
@@ -27,6 +33,7 @@ local function join_results(base_result, update)
   }
 end
 
+-- see ../../run_tests.lua
 local test_script = (Path.new(script_path()):parent():parent() / "run_tests.lua").filename
 
 ---@type neotest.Adapter
@@ -106,16 +113,20 @@ function PlenaryNeotestAdapter.build_spec(args)
     end
   end
 
+  local cwd = assert(vim.loop.cwd())
   local command = vim.tbl_flatten({
     vim.loop.exepath(),
     "--headless",
-    "-u",
-    min_init or "NONE",
-    "-c",
-    "source " .. test_script,
+    "-i", "NONE", -- no shada
+    "-n", -- no swpafile, always in-memory
     "--noplugin",
-    "-c",
-    "lua _run_tests({results = '" .. results_path .. "', file = '" .. async.fn.escape(
+    -- add plenary.nvim to &runtimepath (should be available before init config)
+    "--cmd", ([[lua vim.opt.runtimepath:prepend('%s')]]):format(vim.fn.escape(plenary_dir, " '")),
+    -- Make lua modules at ./lua/ loadable
+    "--cmd", [[lua package.path = 'lua/?.lua;' .. 'lua/?/init.lua;' .. package.path]],
+    "-u", min_init or "NONE",
+    "-c", "source " .. test_script,
+    "-c", "lua _run_tests({results = '" .. results_path .. "', file = '" .. async.fn.escape(
       pos.path,
       "'"
     ) .. "', filter = " .. vim.inspect(filters) .. "})",

run_tests.lua
@@ -1,3 +1,5 @@
+-- The test script to run busted tests in a separate headless nvim process
+
 _G._run_tests = function(args)
   local success, error = pcall(function()
     local filters = args.filter or {}

commit 19e9ddf2ca1182bc87998fba5862fdc7a4154363

fix: importing neotest-plenary causing lua print() broken
Upon require("neotest-plenary"), lua global `print()` (and hence all
lua-based I/O) gets broken. This is because the module "plenary.busted"
overrides and shadows global `print()` as a side-effect.

This is a regression since #10.

Solution: The module "plenary.busted" should never be imported.

lua/neotest-plenary/init.lua
@@ -3,12 +3,6 @@ local Path = require("plenary.path")
 local lib = require("neotest.lib")
 local base = require("neotest-plenary.base")
 
--- the local path to the plenary.nvim plugin installed
----@type string
-local plenary_dir = vim.fn.fnamemodify(
-  debug.getinfo(require("plenary.busted").run).source:match("@?(.*[/\\])"), ":p:h:h:h"
-)
-
 local config = {
   min_init = nil,
 }
@@ -112,6 +106,12 @@ function PlenaryNeotestAdapter.build_spec(args)
       end
     end
   end
+
+  -- the local path to the plenary.nvim plugin installed
+  ---@type string
+  local plenary_dir = vim.fn.fnamemodify(
+    debug.getinfo(require("plenary.path").__index).source:match("@?(.*[/\\])"), ":p:h:h:h"
+  )
 
   local cwd = assert(vim.loop.cwd())
   local command = vim.tbl_flatten({